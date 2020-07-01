2dbd1fc9c00134317466001bed0035c55471c122
GwonHeokjun <ghj1001020@naver.com>
2020-07-01 09:32:02 +0900

    BrowserApp TAG

--- app/src/main/java/com/ghj/browser/BrowserApp.kt
+++ app/src/main/java/com/ghj/browser/BrowserApp.kt
@@ -4,6 +4,9 @@ import androidx.multidex.MultiDexApplication
 
 class BrowserApp : MultiDexApplication() {
 
+    private val TAG : String = "BrowserApp";
+
+
     override fun onCreate() {
         super.onCreate()
     }
